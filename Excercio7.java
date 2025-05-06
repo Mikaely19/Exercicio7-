d50ae42a27d737190b653555d8b3348f2508a488
Mikaely19 <125111409306@ulife.com.br>
2025-05-05 23:37:56 -0300

    Update Excercio7.java

--- Excercio7.java
+++ Excercio7.java
@@ -2,19 +2,16 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         int[] vetor = new int[15];
-
-        // Preenchendo o vetor com números fornecidos pelo usuário
-        System.out.println("Digite 15 números para preencher o vetor:");
+        
+        System.out.println("Digite 15 números:");
         for (int i = 0; i < vetor.length; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             vetor[i] = scanner.nextInt();
         }
 
-        // Pedindo ao usuário um número para contar
-        System.out.print("Digite um número para contar quantas vezes ele aparece no vetor: ");
+        System.out.print("Digite um número para verificar quantas vezes aparece: ");
         int numero = scanner.nextInt();
 
-        // Contando as ocorrências do número no vetor
         int contador = 0;
         for (int valor : vetor) {
             if (valor == numero) {
@@ -22,7 +19,6 @@ public class App {
             }
         }
 
-        // Exibindo o resultado
         System.out.println("O número " + numero + " aparece " + contador + " vezes no vetor.");
 
         scanner.close();
